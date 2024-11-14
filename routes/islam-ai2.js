53318747f281bf0662f9147f0b86e89fb9983bf1
Zoronew <zoro.helloyou@gmail.com>
2024-11-13 18:43:13 -0800

    Update islam-ai2.js

--- routes/islam-ai2.js
+++ routes/islam-ai2.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const fs = require('fs');
-
 // دالة للحصول على الرسائل الافتراضية
 const getDefaultMessages = (userId) => [
     {
