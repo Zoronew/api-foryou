6f2b64645693e831164f91751081d3324d7aea93
Zoronew <zoro.helloyou@gmail.com>
2024-11-23 01:53:01 -0800

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.set('trust proxy', 1)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+
 app.use(cors({
     origin: function(origin, callback) {
         if (!origin) return callback(null, true);
